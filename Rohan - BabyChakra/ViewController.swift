0703446f5650400e6b81ac91a68a450c45580e03
Rohan Dilip Sanap <sanaprohan10@gmail.com>
2017-11-25 12:58:39 +0530

    Common action added and connected to all buttons

--- Rohan - BabyChakra/ViewController.swift
+++ Rohan - BabyChakra/ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        
+    }
+    
 
 
 }
