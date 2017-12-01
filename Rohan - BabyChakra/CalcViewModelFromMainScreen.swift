6053fcaf8a397e54586b358005ddc690ad517a5b
Rohan Dilip Sanap <sanaprohan10@gmail.com>
2017-12-01 16:16:13 +0530

    +/- ignored. Other all buttons tap handled except =

--- Rohan - BabyChakra/CalcViewModelFromMainScreen.swift
+++ Rohan - BabyChakra/CalcViewModelFromMainScreen.swift
@@ -9,37 +9,36 @@
 import UIKit
 
 class ClacViewModelFromMainScreen: CalcViewModel {
-    let model: CalcModel
-    
-    var displayText: Dynamic<String>
-    
-    init() {
-        model = CalcModel()
-        displayText = Dynamic("0")
-    }
+    let errorString = "Error!"
+    var displayText = Dynamic("0")
     
     func buttonTapped(_ button: UIButton) {
         guard let title = button.titleLabel?.text else {
             return
         }
         
+        guard title != "+/-" else {
+            return
+        }
+        
         guard title != "C" else {
-            model.resetModel()
             displayText.value = "0"
             return
         }
         
-        guard title != "+/-" else {
-            if let value = Double(displayText.value) {
-                displayText.value = String(value * -1)
-            }
+        guard title != "=" else {
+            //TODO: evaluate expression
             return
         }
         
-        if displayText.value != "0" {
-            displayText.value = displayText.value + title
+        if displayText.value == "0" || displayText.value == errorString {
+            if title == "." {
+                displayText.value = "0."
+            }else {
+                displayText.value = title
+            }
         }else {
-            displayText.value = title
+            displayText.value = displayText.value + title
         }
     }
 }
